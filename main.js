21c28cbd3b75834409c9348d1694c96f0d8fcc0c
FacundoAguilar <facundoexequiel1996@hotmail.com>
2024-12-10 00:04:24 -0300

    Primera entrega

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
 function loguear(){
 
     let identificar = true;
-    let intentos = 1;
+    let intentos = 3;
 
   do{
     let usuario = prompt("Ingrese su nombre de usuario");
+    let password = prompt("Ingrese su contraseña");
 
-    if(usuario == null || usuario !== "Facundo"){
-        alert("Su usuario es incorrecto vuelva a intentar en 15 minutos");
-        break;
+
+    if(usuario == "" && password == ""){
+        alert("Vuelva a intentar en 15 minutos")
+            break;  
     }
 
-    if(usuario === "Facundo"  || intentos <= 3){
-        let password = prompt("Ingrese su contraseña");
-        if(password == null){
+    if((usuario !== "Facundo" || password !=="123456") && intentos >=3){
+        alert("Su cuenta es incorrecta vuelva a intentarlo");
+        if(intentos < 1){
+            alert("Usted supero los tres intentos");
             break;
         }
+    }
 
-        if(password === "123456"){
-            alert("Bienvenido a su cuenta " + usuario);
-            identificar = false;
-        }
-        else{
-            alert("Contraseña incorrecta");
-            intentos++
-            if(intentos > 3){
+    if(usuario === "Facundo" && password === "123456"){
+        alert("Bienvenido a su cuenta " + usuario)
+        identificar = false;     
+    }
+
+    else{
+            intentos--
+            if(intentos < 1){
                 alert("Usted supero los tres intentos");
                 break;
             }
         }
     }
-
-  }
-
-  while(identificar)
+    
+    while(identificar)
     
 }
-
 loguear()
 
 
